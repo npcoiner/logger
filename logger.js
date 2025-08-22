2642102cfe77372b582e22527056933655b1758e
Nick <nicholascoiner@gmail.com>
2025-08-22 15:00:42 -0700

    Added logs for each level

--- logger.js
+++ logger.js
@@ -15,13 +15,13 @@ export class log {
 		this.level = level;
 		this.outputs = outputs;
 	}
-	output(message, obj = "") {
+	output(level, message, obj = "") {
 		for (let i = 0; i < this.outputs.length; i++) {
 			let x = this.outputs[i];
 
 			switch (x) {
 				case "console":
-					console.log(message, obj);
+					console.log(level, message, obj);
 					break;
 				default:
 					console.log(x, "FATAL LOGGING ERROR: No output specified");
@@ -29,13 +29,31 @@ export class log {
 			}
 		}
 	}
-	info(message, obj) {
-		if (getLevelValue(this.level) >= getLevelValue("INFO")) {
-			this.output(message, obj);
+	debug(message, obj) {
+		if (getLevelValue(this.level) >= getLevelValue("DEBUG")) {
+			this.output("DEBUG", message, obj);
 		}
 	}
-
-
+	info(message, obj) {
+		if (getLevelValue(this.level) >= getLevelValue("INFO")) {
+			this.output("INFO", message, obj);
+		}
+	}
+	warn(message, obj) {
+		if (getLevelValue(this.level) >= getLevelValue("WARN")) {
+			this.output("WARN", message, obj);
+		}
+	}
+	error(message, obj) {
+		if (getLevelValue(this.level) >= getLevelValue("ERROR")) {
+			this.output("ERROR", message, obj);
+		}
+	}
+	fatal(message, obj) {
+		if (getLevelValue(this.level) >= getLevelValue("FATAL")) {
+			this.output("FATAL", message, obj);
+		}
+	}
 }
 
 function getLevelValue(level) {
